871d3d1d038458e2bcd4effe93ac6655e98acc71
lpile <logan.pile@gmail.com>
2019-06-25 14:18:39 -0600

    Add #index and #show actions to TransactionsController

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :customers, only: [:index, :show]
       resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
     end
   end
 end

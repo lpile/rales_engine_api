3f09fa0123c7584fda0dfea10ea4cd5d7edf384b
lpile <logan.pile@gmail.com>
2019-06-25 14:24:32 -0600

    Add #index and #show actions to ItemsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index, :show]
-      resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
     end
   end

526cb76f5892886e4c362049462f4e162e181e6b
lpile <logan.pile@gmail.com>
2019-06-25 22:14:51 -0600

    Refactor spec

--- spec/requests/api/v1/merchants/search_request_spec.rb
+++ spec/requests/api/v1/merchants/search_request_spec.rb
@@ -1,49 +1,46 @@
 require 'rails_helper'
 
 describe "Merchants API Finders," do
-  describe "find parameters(attributes)" do
-    before :each do
-      @merchant = create(:merchant)
-      @expected = {"data": {
-                      "id": "#{@merchant.id}",
-                      "type": "merchant",
-                      "attributes": {
-                        "name": "#{@merchant.name}"
-                      }
-                    }
-                  }
-    end
+  before :each do
+    @merchant = create(:merchant)
+  end
+
+  describe "find parameters by attributes" do
 
     it "can find merchant by its id" do
-
       get "/api/v1/merchants/find?id=#{@merchant.id}"
 
+      merchant = JSON.parse(response.body)["data"]
+
       expect(response).to be_successful
-      expect(response.body).to eq(@expected.to_json)
+      expect(merchant["attributes"]["id"].to_i).to eq(@merchant.id)
     end
 
     it "can find merchant by its name" do
-
       get "/api/v1/merchants/find?name=#{@merchant.name}"
 
+      merchant = JSON.parse(response.body)["data"]
+
       expect(response).to be_successful
-      expect(response.body).to eq(@expected.to_json)
+      expect(merchant["attributes"]["name"]).to eq(@merchant.name)
     end
 
     it "can find merchant by its created_at" do
+      get "/api/v1/merchants/find?#{@merchant.created_at}"
 
-      get "/api/v1/merchants/find?created_by=#{@merchant.created_at}"
+      merchant = JSON.parse(response.body)["data"]
 
       expect(response).to be_successful
-      expect(response.body).to eq(@expected.to_json)
+      expect(merchant["attributes"]["id"]).to eq(@merchant.id)
     end
 
     it "can find merchant by its updated_at" do
+      get "/api/v1/merchants/find?#{@merchant.updated_at}"
 
-      get "/api/v1/merchants/find?created_by=#{@merchant.updated_at}"
+      merchant = JSON.parse(response.body)["data"]
 
       expect(response).to be_successful
-      expect(response.body).to eq(@expected.to_json)
+      expect(merchant["attributes"]["id"]).to eq(@merchant.id)
     end
   end
 end

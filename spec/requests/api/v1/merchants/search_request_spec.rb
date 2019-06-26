e60f5c98b575999aada4f8d722af6074b6949f00
lpile <logan.pile@gmail.com>
2019-06-25 22:55:43 -0600

    Add #index action spec

--- spec/requests/api/v1/merchants/search_request_spec.rb
+++ spec/requests/api/v1/merchants/search_request_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe "Merchants API Finders," do
   before :each do
-    @merchant = create(:merchant)
+    @merchant = create(:merchant, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    @merchant2 = create(:merchant, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
   end
 
   describe "find parameters by attributes" do
-
     it "can find merchant by its id" do
       get "/api/v1/merchants/find?id=#{@merchant.id}"
 
@@ -35,12 +35,54 @@ describe "Merchants API Finders," do
     end
 
     it "can find merchant by its updated_at" do
-      get "/api/v1/merchants/find?#{@merchant.updated_at}"
+      get "/api/v1/merchants/find?updated_at=#{@merchant.updated_at}"
 
       merchant = JSON.parse(response.body)["data"]
 
       expect(response).to be_successful
       expect(merchant["attributes"]["id"]).to eq(@merchant.id)
+    end
+  end
+
+  describe "find all parameters by attributes" do
+    it "can find merchant by its id" do
+      get "/api/v1/merchants/find_all?id=#{@merchant.id}"
+
+      merchants = JSON.parse(response.body)["data"]
+
+      expect(response).to be_successful
+      expect(merchants.count).to eq(1)
+      expect(merchants.class).to eq(Array)
+    end
+
+    it "can find merchant by its name" do
+      get "/api/v1/merchants/find_all?name=#{@merchant.name}"
+
+      merchants = JSON.parse(response.body)["data"]
+
+      expect(response).to be_successful
+      expect(merchants.count).to eq(1)
+      expect(merchants.class).to eq(Array)
+    end
+
+    it "can find merchant by its created_at" do
+      get "/api/v1/merchants/find_all?#{@merchant.created_at}"
+
+      merchants = JSON.parse(response.body)["data"]
+
+      expect(response).to be_successful
+      expect(merchants.count).to eq(2)
+      expect(merchants.class).to eq(Array)
+    end
+
+    it "can find merchant by its updated_at" do
+      get "/api/v1/merchants/find_all?#{@merchant.updated_at}"
+
+      merchants = JSON.parse(response.body)["data"]
+
+      expect(response).to be_successful
+      expect(merchants.count).to eq(2)
+      expect(merchants.class).to eq(Array)
     end
   end
 end

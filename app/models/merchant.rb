9411dc9bf5a0f304950c242ec4e28e3d951d96a8
lpile <logan.pile@gmail.com>
2019-06-26 12:05:54 -0600

    Add #top_merchants_by_revenue(quantity)

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,13 @@ class Merchant < ApplicationRecord
   has_many :items
 
   validates_presence_of :name
+
+  def self.top_merchants_by_revenue(input_quantity)
+    joins(invoices: [:transactions, :invoice_items])
+    .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('revenue DESC')
+    .limit(input_quantity)
+  end
 end

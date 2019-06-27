6ae41f9826e7ab6a6934e195e4b0b7d74b8e6ff5
lpile <logan.pile@gmail.com>
2019-06-27 13:08:04 -0600

    Add Customer relationship endpoint spec

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -1,13 +1,46 @@
 require 'rails_helper'
 
 describe "Customers API:" do
-  describe "Record Endpoints" do
-    before :each do
-      Faker::UniqueGenerator.clear
-      @customer1 = create(:customer, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-      @customer2 = create(:customer, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    end
+  before :each do
+    Faker::UniqueGenerator.clear
 
+    @customer1 = create(:customer, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    @customer2 = create(:customer, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    @merchant1 = create(:merchant, name: "Merchant 1")
+    @merchant2 = create(:merchant, name: "Merchant 2")
+    @merchant3 = create(:merchant, name: "Merchant 3")
+    @merchant4 = create(:merchant, name: "Merchant 4")
+
+    @item1 = create(:item, unit_price: 6666, merchant: @merchant1)
+    @item2 = create(:item, unit_price: 1010, merchant: @merchant2)
+    @item3 = create(:item, unit_price: 3452, merchant: @merchant3)
+    @item4 = create(:item, unit_price: 53023, merchant: @merchant1)
+    @item5 = create(:item, unit_price: 12345, merchant: @merchant3)
+    @item6 = create(:item, unit_price: 3456, merchant: @merchant4)
+
+    @invoice1 = create(:invoice, merchant: @merchant1, customer: @customer1, created_at: "2012-03-27T14:54:05.000Z")
+    @invoice2 = create(:invoice, merchant: @merchant2, customer: @customer1)
+    @invoice3 = create(:invoice, merchant: @merchant3, customer: @customer1, created_at: "2012-03-27T14:54:05.000Z")
+    @invoice4 = create(:invoice, merchant: @merchant1, customer: @customer2, created_at: "2012-03-27T14:54:05.000Z")
+    @invoice5 = create(:invoice, merchant: @merchant4, customer: @customer2)
+
+    @transaction1 = create(:transaction, invoice: @invoice1, result: "success")
+    @transaction2 = create(:transaction, invoice: @invoice2, result: "success")
+    @transaction3 = create(:transaction, invoice: @invoice3, result: "success")
+    @transaction4 = create(:transaction, invoice: @invoice4, result: "success")
+    @transaction5 = create(:transaction, invoice: @invoice5)
+
+    @invoice_item1 = create(:invoice_item, item: @item1, invoice: @invoice1, quantity: 1, unit_price: @item1.unit_price)
+    @invoice_item2 = create(:invoice_item, item: @item4, invoice: @invoice1, quantity: 1, unit_price: @item4.unit_price)
+    @invoice_item3 = create(:invoice_item, item: @item2, invoice: @invoice2, quantity: 7, unit_price: @item2.unit_price)
+    @invoice_item4 = create(:invoice_item, item: @item3, invoice: @invoice3, quantity: 1, unit_price: @item3.unit_price)
+    @invoice_item5 = create(:invoice_item, item: @item5, invoice: @invoice3, quantity: 1, unit_price: @item5.unit_price)
+    @invoice_item6 = create(:invoice_item, item: @item4, invoice: @invoice4, quantity: 1, unit_price: @item4.unit_price)
+    @invoice_item7 = create(:invoice_item, item: @item6, invoice: @invoice5, quantity: 1, unit_price: @item6.unit_price)
+  end
+
+  describe "Record Endpoints" do
     it "sends a list of customers" do
       get "/api/v1/customers"
 
@@ -126,7 +159,29 @@ describe "Customers API:" do
     end
   end
 
-  # Relationship Endpoints
+  describe "Relationship Endpoints" do
+    it "returns a collection of associated transactions" do
+      get "/api/v1/customers/#{@customer1.id}/transactions"
+
+      customers = JSON.parse(response.body)["data"]
+
+      expect(response).to be_successful
+      expect(customers.count).to eq(3)
+      expect(customers.class).to eq(Array)
+      expect(customers[0]["type"]).to eq("transaction")
+    end
+
+    it "returns a collection of associated invoices" do
+      get "/api/v1/customers/#{@customer1.id}/invoices"
+
+      customers = JSON.parse(response.body)["data"]
+
+      expect(response).to be_successful
+      expect(customers.count).to eq(3)
+      expect(customers.class).to eq(Array)
+      expect(customers[0]["type"]).to eq("invoice")
+    end
+  end
 
   # Business Intelligent Endpoints
 end

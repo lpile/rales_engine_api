39a2da2e024b492cce4c326e14ca1a54991796fe
lpile <logan.pile@gmail.com>
2019-06-27 13:08:51 -0600

    Add #index action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       namespace :customers do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/:id/invoices', to: 'invoices#index'
+        get '/:id/transactions', to: 'transactions#index'
       end
 
       namespace :invoice_items do

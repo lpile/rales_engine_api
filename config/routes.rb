86e0ab82084f1e4745e99e30680b6bb97afba17f
lpile <logan.pile@gmail.com>
2019-06-29 12:56:40 -0600

    Add Customers::FavoriteMerchantController

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/:id/invoices', to: 'invoices#index'
         get '/:id/transactions', to: 'transactions#index'
+        get '/:id/favorite_merchant', to: 'favorite_merchant#show'
       end
 
       namespace :invoice_items do

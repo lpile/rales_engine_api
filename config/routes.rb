6d7e5f21e4cdefa3e71f1f3e0f130aea761c895b
lpile <logan.pile@gmail.com>
2019-06-29 20:54:11 -0600

    Add #index action to Items::MostRevenueController

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/merchant', to: 'merchants#show'
+        get '/most_revenue', to: 'most_revenue#index'
       end
 
       namespace :merchants do

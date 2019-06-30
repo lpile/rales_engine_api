e6e4b085632c16ae0c87f61b5d734b9007bc6021
lpile <logan.pile@gmail.com>
2019-06-29 20:54:27 -0600

    Add #top_items_by_revenue(input_quantity)

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates_presence_of :name, :description, :unit_price
+
+  def self.top_items_by_revenue(input_quantity)
+    joins(:invoice_items, invoices: :transactions)
+    .select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('revenue DESC')
+    .limit(input_quantity)
+  end
 end

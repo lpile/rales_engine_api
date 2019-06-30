443abdf76f780de4ea1e9596076a1cff4e0e7adb
lpile <logan.pile@gmail.com>
2019-06-29 20:26:51 -0600

    Rename Merchants::DateController to Merchants::RevenueController

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Merchants::DateController < ApplicationController
+class Api::V1::Merchants::RevenueController < ApplicationController
   # total revenue on date
   def show
     render json: TotalRevenueSerializer.new(Merchant.total_revenue_on_date(params["date"]))
--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
         get '/:id/items', to: 'items#index'
         get '/most_revenue', to: 'most_revenue#index'
         get '/most_items', to: 'most_items#index'
-        get '/revenue', to: 'date#show'
+        get '/revenue', to: 'revenue#show'
       end
 
       namespace :transactions do

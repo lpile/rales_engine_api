7c5090f7bbf3ed35cbc94040e2ea64f185b2526b
lpile <logan.pile@gmail.com>
2019-06-30 00:22:02 -0600

    Add #show action to Merchants::FavoriteCustomerController

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/:id/invoices', to: 'invoices#index'
         get '/:id/items', to: 'items#index'
+        get '/:id/favorite_customer', to: 'favorite_customer#show'
         get '/:id/revenue', to: 'revenue#show'
         get '/most_items', to: 'most_items#index'
         get '/most_revenue', to: 'most_revenue#index'

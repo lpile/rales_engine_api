d303744f1302a61d33aef29566c647daa2459205
lpile <logan.pile@gmail.com>
2019-06-30 13:00:05 -0600

    Refactor items factorybot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,18 @@ FactoryBot.define do
   factory :item do
     name { Faker::Commerce.unique.product_name }
     sequence :description {|n| "Description #{n}"}
-    unit_price { rand(100..100000) }
     merchant
+
+    factory :expensive_item do
+      unit_price { rand(80000..100000) }
+    end
+
+    factory :moderate_item do
+      unit_price { rand(40000..60000) }
+    end
+
+    factory :cheap_item do
+      unit_price { rand(10000..20000) }
+    end
   end
 end
